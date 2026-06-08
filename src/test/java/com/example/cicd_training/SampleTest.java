050b168c3527df5654ba149de5132c0c6be3292c
中町雷我 <nakamachi.raiga@compass-japan.com>
2026-06-08 14:45:24 +0900

    Add sample test

---
+++ src/test/java/com/example/cicd_training/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example.cicd_training;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+    @Test
+    void サンプルテスト() {
+        assertEquals(2, 1 + 1);
+    }
+}
